cfea0b5313e9ed2803deb6805d2738df6e7ecd3c
azte <cuellar.uk@gmail.com>
2014-04-25 13:44:21 -0500

    First Comit
    Primer Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Encuesta SUPERISSSTE</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+	</head>
+<body>
+<div class="wrapper">
+<style type="text/css">
+ body{background-image:url('img/background.png');}
+</style>	
+
+	<div id="content">
+
+		<header>
+
+			<figure class="logos"><img src="img/superissste_logo.jpg"></figure>
+			<figure class="logos"><img src="img/kio_logo.png"></figure>
+			<p id="mesa">MESA DE AYUDA<p>
+			<p id="issste">SUPERISSSTE</p>
+		</header>
+
+		<section>
+			<h2>Encuesta de Satisfacción</h2>
+			<figure><img src="img/service_girl.png"></figure>
+			<p>-Le pedimos tener a la mano su ticket-</p>
+			<button>TOMAR ENCUESTA</button>
+		</section>
+
+		<footer>
+				<p>Av San Fernando 15 Col. Toriello Guerra, Tlalpan, 14070 México, D.F.</p> 
+				<p> Tel. 54476200 Ext: 99678 | 01 800 123 22 22</p>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ llamadas.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Encuesta SUPERISSSTE</title>
+		<link rel="stylesheet" type="text/css" href="css/forms.css">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+	</head>
+<body>
+<!-- <div class="wrapper">
+<style type="text/css">
+ body{background-image:url('img/background.png');}
+</style>	
+ -->
+
+	<header>
+	</header>
+	<div id="form_content">
+		<figure><img src="#"></figure>
+		<div id="survey_message">
+		</div>
+		<form name="enviar" method="POST">
+			
+			<div id="data_form">
+				<table>
+					<tr>
+						<td>FECHA:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="fecha"></td>
+					</tr>
+					<tr>
+						<td>Nombre:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="nombre"></td>
+					</tr>
+					<tr>
+						<td>Unidad de Venta:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="tienda"></td>
+					</tr>
+					<tr>
+						<td>Ticket:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="ticket"></td>
+					</tr>
+
+				</table>
+
+			</div>
+			<div id="questions">
+				<table id="questions_table">
+					<tr><td class="td_questions"colspan=5>1. ¿La atención del ejecutivo de Mesa de Ayuda que atendió su reporte fue:?</td></tr>
+					<tr>
+						<td><input type="radio" name="answer1" value="Excelente">Excelente</td>
+						<td><input type="radio" name="answer1" value="Bueno">Bueno</td>
+						<td><input type="radio" name="answer1" value="Regular">Regular</td>
+						<td><input type="radio" name="answer1" value="Malo">Malo</td>
+						<td><input type="radio" name="answer1" value="Muy Malo">Muy Malo</td>
+					</tr>
+					<tr><td class="td_questions"colspan=5>2. ¿La solución que brindo el especialista / grupo de Asistencia fue:?</td></tr>
+					<tr>
+						<td><input type="radio" name="answer2" value="Excelente">Excelente</td>
+						<td><input type="radio" name="answer2" value="Bueno">Bueno</td>
+						<td><input type="radio" name="answer2" value="Regular">Regular</td>
+						<td><input type="radio" name="answer2" value="Malo">Malo</td>
+						<td><input type="radio" name="answer2" value="Muy Malo">Muy Malo</td>
+					</tr>
+
+					<tr><td class="td_questions" colspan=5>3. ¿Cómo evalúa el tiempo en que fue resuelto su ticket?</td></tr>
+					<tr>
+						<td><input type="radio" name="answer3" value="Excelente">Excelente</td>
+						<td><input type="radio" name="answer3" value="Bueno">Bueno</td>
+						<td><input type="radio" name="answer3" value="Regular">Regular</td>
+						<td><input type="radio" name="answer3" value="Malo">Malo</td>
+						<td><input type="radio" name="answer3" value="Muy Malo">Muy Malo</td>
+					</tr>
+
+					<tr><td class="td_questions" colspan=5>4. ¿La calidad global del servicio fue?</td></tr>
+					<tr>
+						<td><input type="radio" name="answer4" value="Excelente">Excelente</td>
+						<td><input type="radio" name="answer4" value="Bueno">Bueno</td>
+						<td><input type="radio" name="answer4" value="Regular">Regular</td>
+						<td><input type="radio" name="answer4" value="Malo">Malo</td>
+						<td><input type="radio" name="answer4" value="Muy Malo">Muy Malo</td>
+					</tr>
+
+					<tr><td class="td_questions" colspan=5>5. Comentarios Adicionales?</td></tr>
+					<tr>
+						<td colspan=5><textarea name="comentarios" rows="3" cols="65"></textarea></td>
+						
+					</tr>
+
+
+
+
+				</table>
+
+
+			
+			</div>
+				<button>ENVIAR ENCUESTA</button>
+
+		</form>
+
+		<footer></footer>
+
+	</div>
+
+
+
+
+</body>
+</html>
